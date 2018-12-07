65f2435b3e137a581917dc0c03d6b8584a4226e1
Justin Kotalik <jukotali@microsoft.com>
2018-12-06 18:10:45 -0800

    Feedback:

--- src/Installers/Windows/buildWithFix.ps1
+++ src/Installers/Windows/buildWithFix.ps1
@@ -3,7 +3,7 @@ mkdir obj -ErrorAction Ignore
 Invoke-WebRequest https://dotnetcli.blob.core.windows.net/dotnet/aspnetcore/Runtime/2.2.0/aspnetcore-runtime-internal-2.2.0-win-x64.zip -o .\obj\dotnetx64.zip
 Invoke-WebRequest https://dotnetcli.blob.core.windows.net/dotnet/aspnetcore/Runtime/2.2.0/aspnetcore-runtime-internal-2.2.0-win-x86.zip -o .\obj\dotnetx86.zip
 
-.\build.ps1 -x64 .\obj\dotnetx64.zip -x86 .\obj\dotnetx86.zip -IsFinalBuild -BuildNumber $env:BUILD_NUMBER`
+$PSScriptRoot\build.ps1 -x64 .\obj\dotnetx64.zip -x86 .\obj\dotnetx86.zip -IsFinalBuild -BuildNumber $env:BUILD_NUMBER`
 -SignType Real`
 /p:MicrosoftNetCoreAppPackageVersion=2.2.0`
 /p:MicrosoftAspNetCoreAspNetCoreModulePackageVersion=2.2.0`
