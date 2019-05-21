47c1e3f399006c8369fe98b2bae5c37c0a391ac6
Ryan Brandenburg <rybrande@microsoft.com>
2019-05-21 16:23:35 -0700

    Test fixes

--- src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Startup.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/WebApi-CSharp/Startup.cs
@@ -48,11 +48,13 @@ namespace Company.WebApplication1
 #endif
             services.AddControllers()
                 .AddNewtonsoftJson();
+#if (Swashbuckle)
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
             });
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
