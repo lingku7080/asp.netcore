2fc22b3a5993b360639d8c81cc1c9c332f1efb49
Stephen Halter <halter73@gmail.com>
2019-08-18 16:21:13 -0700

    WIP

---
+++ src/Servers/Kestrel/tools/http2cat/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace http2cat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
